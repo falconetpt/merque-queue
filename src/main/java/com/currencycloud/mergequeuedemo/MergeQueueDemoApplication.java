0a1f33d12b6ba16450395ff5769d364216319267
Ricardo Gomes <rgomes@visa.com>
2024-10-09 15:22:59 +0100

    Test

--- src/main/java/com/currencycloud/mergequeuedemo/MergeQueueDemoApplication.java
+++ src/main/java/com/currencycloud/mergequeuedemo/MergeQueueDemoApplication.java
@@ -3,6 +3,7 @@ package com.currencycloud.mergequeuedemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test
 @SpringBootApplication
 public class MergeQueueDemoApplication {
 
