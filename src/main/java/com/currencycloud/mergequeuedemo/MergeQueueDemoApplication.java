3be84264d214bdf33bb9e010566c2cd8c33bfb45
Ricardo Gomes <rgomes@visa.com>
2024-10-09 15:14:22 +0100

    Adds all

---
+++ src/main/java/com/currencycloud/mergequeuedemo/MergeQueueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.currencycloud.mergequeuedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeQueueDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MergeQueueDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/currencycloud/mergequeuedemo/MergeQueueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencycloud.mergequeuedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeQueueDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
